6a50164a662860a3574ad0ab772ec35dfc06f1bb
playbjyou <playbjyou@naver.com>
2022-05-07 22:11:48 +0900

    [add] sungkyul maps layout good good

---
+++ app/src/main/java/com/example/realitymapper/HakSaengActivity.java
@@ -0,0 +1,14 @@
+package com.example.realitymapper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HakSaengActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hak_saeng);
+    }
+}
--- app/src/main/java/com/example/realitymapper/MainActivity.java
+++ app/src/main/java/com/example/realitymapper/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.realitymapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnMap;
+    Button btnInfo;
+    Button btnImprovements;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnInfo = (Button) findViewById(R.id.btnInfo);
+
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
