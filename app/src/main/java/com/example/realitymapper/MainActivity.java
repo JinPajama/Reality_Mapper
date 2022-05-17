84c484bc4594b2b12faf4ef85814147c3b8902a9
kang <superb5570@gmail.com>
2022-05-18 00:18:01 +0900

    검색기능 추가 (리스트작성해야함)

--- app/src/main/java/com/example/realitymapper/MainActivity.java
+++ app/src/main/java/com/example/realitymapper/MainActivity.java
@@ -1,22 +1,97 @@
 package com.example.realitymapper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnMap; /*지도*/
     Button btnInfo; /*건물정보*/
     Button btnImprovements;  /*개선사항*/
+    private List<String> list;
+    private ListView listView;
+    private EditText editSearch;
+    private SearchAdapter adapter;
+    private ArrayList<String> arraylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editSearch = (EditText) findViewById(R.id.editSearch);
+        listView = (ListView) findViewById(R.id.listView);
+
+        list = new ArrayList<String>();
+
+        settingList();
+
+        arraylist = new ArrayList<String>();
+        arraylist.addAll(list);
+
+        adapter = new SearchAdapter(list, this);
+
+        listView.setAdapter(adapter);
+
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String text = editSearch.getText().toString();
+                search(text);
+            }
+        });
+
+
+    }
+
+    public void search(String charText) {
+
+        list.clear();
+
+        if (charText.length() == 0) {
+            list.addAll(arraylist);
+        }
+        else
+        {
+
+            for(int i = 0;i < arraylist.size(); i++)
+            {
+
+                if (arraylist.get(i).toLowerCase().contains(charText))
+                {
+                    list.add(arraylist.get(i));
+                }
+            }
+        }
+
+        adapter.notifyDataSetChanged();
+    }
+
+    private void settingList(){
+        list.add("강승규 01079143404");
+        list.add("010");
+        list.add("01051343");
+
 
         btnInfo = (Button) findViewById(R.id.btnInfo);
 
